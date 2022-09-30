d9cb52b7f25278523237060c94b286f631160430
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-09-30 10:09:03 +0700

    number one fix bug

--- convert.js
+++ convert.js
@@ -1,37 +1,18 @@
 function ConvertNumberToText(n) {
-    if ( n < 0 ){return "Minus " + ConvertNumberToText(-n);}
-   else if ( n == 0 ){return "";}
+   if ( n == 0 ){return "";}
    else if ( n <= 19 ){
-    const arr =  ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", 
-    "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-    "Seventeen", "Eighteen", "Nineteen"]
-      return  arr[n-1];
-   }else if ( n <= 99 ){
-    const array =  ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", 
-    "Eighty", "Ninety"]
-
-     const result =   array[Math.floor(n/10)-2] + " " + ConvertNumberToText(n % 10);
-
-      return result
+    const arr =["satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", 
+                "sembilan", "sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", 
+                "tujuh belas", "delapan belas", "sembilan belas"]
+    return  arr[n-1]+ " ";
    }
-   else if ( n <= 199 )
-      return "One Hundred " + ConvertNumberToText(n % 100);
-   else if ( n <= 999 )
-      return ConvertNumberToText(n / 100) + "Hundreds " + ConvertNumberToText(n % 100);
-   else if ( n <= 1999 )
-      return "One Thousand " + ConvertNumberToText(n % 1000);
-   else if ( n <= 999999 )
-      return ConvertNumberToText(n / 1000) + "Thousands " + ConvertNumberToText(n % 1000);
-   else if ( n <= 1999999 )
-      return "One Million " + ConvertNumberToText(n % 1000000);
-   else if ( n <= 999999999)
-      return ConvertNumberToText(n / 1000000) + "Millions " + ConvertNumberToText(n % 1000000);
-   else if ( n <= 1999999999 )
-      return "One Billion " + ConvertNumberToText(n % 1000000000);
-   else 
-      return ConvertNumberToText(n / 1000000000) + "Billions " + NumberToText(n % 1000000000);
-    
+   else if ( n <= 99 )return ConvertNumberToText(Math.floor(n/10)) +"puluh" +" " + ConvertNumberToText(n % 10);
+   else if ( n <= 199 ) return "seratus " + ConvertNumberToText(n % 100);
+   else if ( n <= 999 ) return ConvertNumberToText(Math.floor(n/100)) + "ratus " + ConvertNumberToText(n % 100);
+   else if ( n <= 1999 ) return "seribu " + ConvertNumberToText(n % 1000);
+   else if ( n <= 999999 ) return ConvertNumberToText(Math.floor(n/1000)) + "ribu " + ConvertNumberToText(n % 1000);  
 }
 
-
 console.log(ConvertNumberToText(12))
+console.log(ConvertNumberToText(2012))
+console.log(ConvertNumberToText(999999))
